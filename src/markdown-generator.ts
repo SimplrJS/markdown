2bf9fd4675109db368f8bfad3d66a457af3404d8
Deividas Bakanas <deividas@quatrodev.com>
2017-12-21 20:09:33 +0200

    Vertical bar symblol (`|`) escape strategy changed.

--- src/markdown-generator.ts
+++ src/markdown-generator.ts
@@ -236,6 +236,8 @@ export namespace MarkdownGenerator {
      * @see https://daringfireball.net/projects/markdown/syntax#backslash
      */
     export function EscapeString(text: string): string {
-        return text.replace(/[\\\`\*\_\{\}\[\]\(\)\#\+\-\.\!\|]/g, "\\$&");
+        return text
+            .replace(/[\\\`\*\_\{\}\[\]\(\)\#\+\-\.\!]/g, "\\$&")
+            .replace(/\|/, "&#124");
     }
 }
